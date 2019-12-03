b07e17deb839113ea3c21cb97b113322575bf032
Henrique <henriqueator@gmail.com>
2019-12-03 00:12:58 -0300

    seting up controllers

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = function () {
+    
+}
+
+exports.logout = function () {
+    
+}
+
+exports.register = function (req, res) {
+    res.send('Obrigado por registrar')
+}
+
+exports.home = function (req, res) {
+    res.render('home-guest')
+    }
