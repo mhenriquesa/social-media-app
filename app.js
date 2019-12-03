aa0141b4f0db9b6f11267668f59f57dcf5f77e26
Henrique <henriqueator@gmail.com>
2019-12-03 03:16:40 -0300

    Configuracao do acesso aos dados do user e Constructor

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express') // *1
+const express = require('express')
 const app = express()
 const router = require('./router')
 
-app.use(express.static('public'))
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))  // acesso a dados do user pelo body do elemento
+app.use(express.json())
 
-app.use('/', router)
+app.use(express.static('public'))               //permitir acesso a pasta public
+app.set('views', 'views')
+app.set('view engine', 'ejs')                   
+
+app.use('/', router)                            // Link do Router
 
 app.listen(3000)
