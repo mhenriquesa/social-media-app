46fa16a3a91c5a449d16831d011f4775324f84bb
Henrique <henriqueator@gmail.com>
2019-12-02 22:20:09 -0300

    adicionado HTML para o /

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
+
+const express = require('express') // *1
 const app = express()
 
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-res.send('Welcome')
+res.render('home-guest')
 })
 
 app.listen(3000)
