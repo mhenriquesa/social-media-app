c6bc1cb97d217669630593f3fe86d44df22cf55a
M. Henrique <henriqueator@gmail.com>
2019-12-17 07:33:45 -0300

    Proteção ao front-end usando domPurify; HTML dinamico para as buscas;

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import domPurify from 'dompurify'
+
 
 export default class Search {
 
@@ -43,7 +45,7 @@ export default class Search {
         clearTimeout(this.typingWaitTimer)
         this.showLoaderIcon()
         this.hideResultsArea()
-        this.typingWaitTimer = setTimeout(() => this.sendRequest(), 750)
+        this.typingWaitTimer = setTimeout(() => this.sendRequest(), 1000)
       }
 
       this.previousValue = value
@@ -60,26 +62,17 @@ export default class Search {
     
     renderResultHTML(posts) {
       if (posts.length) {
-        this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-        <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
-      </div>`    
+        console.log(posts.length)
+        this.resultsArea.innerHTML = domPurify.sanitize(`<div class="list-group shadow-sm">
+        <div class="list-group-item active"><strong>Resultado da busca</strong> (${ posts.length > 1 ? ` ${posts.length} itens encontrados` : ' 1 item encontrado ' } ) </div>
+        ${posts.map( post=> {
+          let postDate = new Date(post.createdDate)
+          return `<a href="/post/${post._id}" class="list-group-item list-group-item-action">
+          <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
+          <span class="text-muted small">Por ${post.author.username} em ${postDate.getDate()}/${postDate.getMonth() + 1}/${postDate.getFullYear()} </span>
+        </a>`
+        }).join('')}
+      </div>`)    
       } else {
         this.resultsArea.innerHTML = `<p class='alert alert-danger text-center shadow-sm' >Sinto muito. Não encontramos nenhum resultado para sua busca</p>`
       }
